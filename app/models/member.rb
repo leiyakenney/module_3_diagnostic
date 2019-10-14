258afd2f536dd149308109fde8a0d1b747ffbbec
Leiya Kenney <leiya.kenney@gmail.com>
2019-10-14 10:25:40 -0600

    Updates member model/test to include house attribute

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,7 +1,8 @@
 class Member
-  attr_reader :name
+  attr_reader :name, :house
 
   def initialize(attributes = {})
     @name = attributes[:name]
+    @house = attributes[:house]
   end
-end 
+end
--- spec/models/member_spec.rb
+++ spec/models/member_spec.rb
@@ -3,12 +3,14 @@ require 'rails_helper'
 describe Member do
   it "exists" do
     attrs = {
-      name: "Hermione Granger"
+      name: "Hermione Granger",
+      house: "Gryffindor"
     }
 
     member = Member.new(attrs)
 
     expect(member).to be_a Member
     expect(member.name).to eq("Hermione Granger")
+    expect(member.house).to eq("Gryffindor")
   end
 end
