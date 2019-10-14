bbdcd8d2189be151b1b75e061553df0d11470cc0
Leiya Kenney <leiya.kenney@gmail.com>
2019-10-14 11:15:01 -0600

    Takes out commented out code

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -6,11 +6,9 @@ class SearchController < ApplicationController
   private
 
   def search_members(state)
-    #grab relevant data from external api
     json_response = conn.get("/v1/characters?house=Gryffindor&key=#{ENV['POTTER_API_KEY']}")
     member_data = JSON.parse(json_response.body, symbolize_names: true)
 
-    #format data by changing it into an object
     members = member_data.map do |member_hash|
       Member.new(member_hash)
     end
