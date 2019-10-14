4910c1c72d8b1d15e4822dc162c0b86bd4b681c4
Leiya Kenney <leiya.kenney@gmail.com>
2019-10-14 10:41:58 -0600

    Un-separates all methods out to try to make it work

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,7 +1,23 @@
 class SearchController < ApplicationController
   def index
-    render locals: {
-      facade: HouseMemberSearchResults.new(params[:house])
-    }
+    @members = search_members(params[:house])
+  end
+
+  def search_members(state)
+    #grab relevant data from external api
+    json_response = conn.get("/v1/characters?key=#{ENV['POTTER_API_KEY']}")
+    member_data = JSON.parse(json_response.body, symbolize_names: true)
+
+    #format data by changing it into an object
+    members = member_data.map do |member_hash|
+      Member.new(member_hash)
+    end
+  end
+
+  def conn
+    Faraday.new(
+      url: "https://www.potterapi.com/v1/",
+      headers: {'X-API-KEY' => ENV["POTTER_API_KEY"]}
+    )
   end
 end
