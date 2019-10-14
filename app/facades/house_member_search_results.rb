8a4697a88d6c88083cfc86b975c84fd887b32c57
Leiya Kenney <leiya.kenney@gmail.com>
2019-10-14 09:54:13 -0600

    Adds houses method to facade

--- app/facades/house_member_search_results.rb
+++ app/facades/house_member_search_results.rb
@@ -4,10 +4,24 @@ class HouseMemberSearchResults
   end
 
   def house_name
-    house.name
+    house
+  end
+
+  def member_count
+    members.count
   end
 
   def house
-    conn = Faraday.new(url:)
+    conn = Faraday.new(url: "https://www.potterapi.com/v1/") do |faraday|
+      faraday.headers["X-API-KEY"] = ENV['POTTER_API_KEY']
+      faraday.adapter Faraday.default_adapter
+    end
+
+    response = conn.get("/characters")
+
+    member_search_data = JSON.parse(response.body, symbolize_names: true)[:results]
   end
+
+  private
+  attr_reader :house
 end
