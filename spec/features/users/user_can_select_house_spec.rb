9035544fcf0ce14832edcce54c64ac0117d8466d
Leiya Kenney <leiya.kenney@gmail.com>
2019-10-14 10:58:51 -0600

    Updates Gryffindor members to be accurate

--- spec/features/users/user_can_select_house_spec.rb
+++ spec/features/users/user_can_select_house_spec.rb
@@ -5,15 +5,16 @@ feature 'A User can select a house' do
     visit root_path
     select "Gryffindor", :from => :house
     click_button 'Search For Members'
+    member = Member.new(@name: "Hermione Granger", @house: )
   end
   scenario 'can see a total of the number of members for that house' do
 
     expect(current_path).to eq(search_path)
-    expect(page).to have_content("Total Gryffindor Members: 18")
-    expect(house_list).to have(18).things
+    expect(page).to have_content("Total Gryffindor Members: 41")
   end
 
   scenario 'can see a list of all members for that house with their name, role, house, and the patronus' do
+    expect(page).to have_content(member.name)
   end
 end
 
